fef6e3ddc49463f6d079ae053fc48f6bff3a7c9f
Brian OToole <botoole518@gmail.com>
2017-04-15 18:20:22 +0000

    rm unncessary modules

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var path    = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+
 
 var index = require('./routes/index');
 //var users = require('./routes/users');
@@ -22,9 +20,6 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 // assets path
 app.use(express.static(path.join(__dirname, '/dist')));
 
@@ -41,7 +36,6 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   // render the error page
   res.render('error', {
